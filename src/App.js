66f996048554ef6c9924f43d8f06b6b6942e9c4f
Sadek Al Shaar <sadekalshaar018@gmail.com>
2018-09-16 13:37:50 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './header/navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="App__nav">
+            <Nav />
+          </div>
+          <div className="App__logo">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
         </header>
+
       </div>
     );
   }
---
+++ src/header/navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Nav extends Component {
+  render() {
+    return <div>
+      <h1>Hello from nav bar</h1>
+    </div>
+  }
+}
+
+export default Nav;
