e490202aff9b34a5db5bf16383393d3a292ed455
Sadek Al Shaar <sadekalshaar018@gmail.com>
2018-09-29 19:52:08 +0200

    app update

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import '../../App.css';
+import Nav from '../header/navbar';
+
 
 export default class Footer extends React.Component {
   render(){
     return(
       <div id="contact">
-        <p>hello from footer</p>
+        <div className="footer">
+          <div className="footer__inner">
+            <Nav />
+          </div>
+        </div>
+        <small className="footer__copy"><p>@copy writer</p></small>
       </div>
     );
   }
