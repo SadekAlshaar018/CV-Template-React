6f05083803fb74fabe27d31382c3161becee1775
Sadek Al Shaar <sadekalshaar018@gmail.com>
2018-10-01 20:30:19 +0200

    delete modernizr JS

--- src/component/cover/cover.js
+++ src/component/cover/cover.js
@@ -8,6 +8,7 @@ export default class Cover extends React.Component {
     return(
       <div className="cover cb-slideshow">
         <ul>
+            <li><span className="cover"><h2>Crafting visually stunning experiences.</h2></span></li>
              <li><span className="cover"></span></li>
              <li><span className="cover"></span></li>
              <li><span className="cover"></span></li>
